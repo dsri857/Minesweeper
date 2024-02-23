dbe74408d060bb427712c7df1e0d8e685fc6ef35
dsri857 <113185686+dsri857@users.noreply.github.com>
2024-02-23 13:06:00 +0530

    Add files via upload

---
+++ mine_sw/build/CMakeFiles/nibbles.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nibbles.dir/main.cpp.obj"
+  "CMakeFiles/nibbles.dir/main.cpp.obj.d"
+  "libnibbles.dll.a"
+  "nibbles.exe"
+  "nibbles.exe.manifest"
+  "nibbles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nibbles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
